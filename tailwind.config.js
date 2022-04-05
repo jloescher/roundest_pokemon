01bacc401c42cd4954c1582fa893f46a86913936
jloescher <jonathan@jloescher.com>
2022-04-04 21:22:03 -0400

    Dumped boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkmode: false,
   theme: {
     extend: {},
   },
